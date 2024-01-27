3ace20a46339426805b94b335b971e564a0266e5
Tobiasz123 <tobiasz01ipo@gmail.com>
2024-01-27 20:55:35 +0100

    loginPage[Complete]

--- check/views.py
+++ check/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-def home(request):
+@login_required(login_url='login')
+def loginPage(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        passwd = request.POST.get('passwd')
+        
+        user = authenticate(request, username=username, password = passwd)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
     return render(request, "check/login.html")
+
+def homePage(request):
+    return render(request, "check/home.html")
 # Create your views here.
--- monit/urls.py
+++ monit/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from check import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('check.urls'))
+    path('', views.loginPage, name="check"),
+    path('home/', views.homePage, name='home'),
 ]
