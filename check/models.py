5e2c612664789fcc2b9bbaef23e4a22089a30c91
Tobiasz123 <tobiasz01ipo@gmail.com>
2024-01-28 18:53:15 +0100

    sitesFirstMigrations

--- check/models.py
+++ check/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
     
 class Sites(models.Model):
     link = models.URLField(max_length=200)
-    userId = models.CharField(max_length=100)
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)
     @admin.display(
         boolean= True,
         ordering='userId',
