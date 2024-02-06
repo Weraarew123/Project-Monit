0a8e9e192cb706959ce948263e228c2336615eb4
Tobiasz123 <tobiasz01ipo@gmail.com>
2024-02-06 14:57:39 +0100

    UsersSitesDisplaying

--- check/tasks.py
+++ check/tasks.py
@@ -1,7 +1,19 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
+from django.shortcuts import get_object_or_404, redirect
+import requests
+from .models import Sites
 
 logger = get_task_logger(__name__)
+
+@shared_task
+def http_response(link):
+    r = requests.get(link)
+    stat = r.status_code
+    obj = get_object_or_404(Sites, link=link)
+    obj.status = stat
+    obj.save()
+    return redirect('home')
 
 @shared_task
 def add(x, y):
--- check/views.py
+++ check/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .tasks import add
+from .tasks import http_response, add
+import requests
+from .models import Sites
 
 def homePage(request):
-    return render(request, "check/home.html")
+    fullLink = {}
+    links = Sites.objects.filter(user_owner=request.user)
+    if links.exists():
+        for link in links:
+            r=requests.get(link.link)
+            stat = r.status_code
+            fullLink.update({link.link:stat})
+            http_response.delay(link.link)
+    context ={
+        'links':fullLink,
+    }
+    return render(request, "check/home.html", context)
 
 def tasker(request):
     return HttpResponse(add.delay(4,4))
